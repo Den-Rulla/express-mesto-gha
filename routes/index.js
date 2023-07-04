d15b9379329740c610a7a01e98338cb330e6e165
Denis Rulla <rda.ssi@yandex.ru>
2023-07-04 09:38:06 +0300

    пр13 на первую проверку

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const { NOT_FOUND_ERROR } = require('../utils/errors');
 
 router.use(usersRouter);
 router.use(cardsRouter);
 
-router.use('*', (req, res) => res.status(404).send({ message: 'Page Not Found' }));
+router.use('*', (req, res) => res.status(NOT_FOUND_ERROR).send({ message: 'Page Not Found' }));
 
 module.exports = router;
