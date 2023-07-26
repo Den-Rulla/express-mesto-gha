0a0254e2e02663d93a48d3296844f2e3e729b90d
Denis Rulla <rda.ssi@yandex.ru>
2023-07-26 18:18:11 +0300

    пр14 test9 commit

--- utils/constants.js
+++ utils/constants.js
@@ -7,8 +7,9 @@ const NOT_FOUND_ERROR = 404;
 const CONFLICT_ERROR = 409;
 const INTERNAL_SERVER_ERROR = 500;
 
-const regExpUrl = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\=]*)$/;
-const regExpEmail = /^\S+@\S+\.\S+$/;
+// eslint-disable-next-line no-useless-escape
+const regExpUrl = /^https?:\/\/[\w\-\.\/~:\?\#\[\]@!$&'\(\)\*\+,;=]+[\-\.\/~:\?\#\[\]@!$&'\(\)\*\+,;=]{1}[\w\-\.\/~:\?\#\[\]@!$&'\(\)\*\+,;=]+[#\/]?$/;
+// const regExpEmail = /^\S+@\S+\.\S+$/;
 
 module.exports = {
   OK_CODE,
@@ -20,5 +21,5 @@ module.exports = {
   CONFLICT_ERROR,
   INTERNAL_SERVER_ERROR,
   regExpUrl,
-  regExpEmail,
+  // regExpEmail,
 };
