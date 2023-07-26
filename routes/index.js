73c17525d35d9b18feeab708308d67f030ded4b7
Denis Rulla <rda.ssi@yandex.ru>
2023-07-26 17:07:09 +0300

    пр14 test7 commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
-const signInRouter = require('./signIn');
-const signUpRouter = require('./signUp');
+const signinRouter = require('./signin');
+const signupRouter = require('./signup');
 const auth = require('../middlewares/auth');
 const NotFoundErr = require('../errors/NotFoundErr');
 
-router.use(signInRouter);
-router.use(signUpRouter);
+router.use(signinRouter);
+router.use(signupRouter);
 
 router.use(auth);
 
